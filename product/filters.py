4deea260f6a0461ace007b2abe42577727012da2
Celsea Mega <celseamega21@gmail.com>
2025-02-26 20:39:50 +0700

    product search with django_filters

---
+++ product/filters.py
@@ -0,0 +1,13 @@
+import django_filters
+from .models import Products
+
+class ProductFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='icontains')
+    store = django_filters.CharFilter(lookup_expr='icontains')
+    min_price = django_filters.NumberFilter(field_name='price', lookup_expr='gte')
+    max_price = django_filters.NumberFilter(field_name='price', lookup_expr='lte')
+    category = django_filters.CharFilter(field_name='category__name', lookup_expr='icontains')
+    
+    class Meta:
+        model = Products
+        fields = ['name', 'store', 'min_price', 'max_price', 'category']

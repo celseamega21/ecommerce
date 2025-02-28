27e933e5f889936016fe2a35a9466271f30cd8a6
Celsea Mega <celseamega21@gmail.com>
2025-03-01 00:46:57 +0700

    modified requirements

--- account/models.py
+++ account/models.py
@@ -12,4 +12,3 @@ class CustomUser(AbstractUser):
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
     phone = models.CharField(max_length=15, blank=True)
     address = models.TextField(blank=True)
-    is_verified = models.BooleanField(default=False)
